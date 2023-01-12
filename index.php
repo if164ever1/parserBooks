5cea89f8dc5271df68068084a7ca0a15fc466785
if164ever1 <acere5ihor@gmail.com>
2023-01-12 22:33:55 +0100

    create function for get all links

--- index.php
+++ index.php
@@ -4,12 +4,24 @@ $url = "https://library.fimsschools.com/all-pdf-books/";
 
 $html = file_get_html($url);
 $array_links = [];
+global $pdf_links_array;
 
 function getBooksLinksFromPage($url)
 {
     $html = file_get_html($url);
     foreach($html->find('a.elementor-post__thumbnail__link') as $link){
-        echo $link->href . PHP_EOL;
+        // echo $link->href . PHP_EOL;
+        savePdfFile($link->href);
+    }
+}
+
+function savePdfFile($url)
+{
+    $html = file_get_html($url);
+    foreach($html->find('a.elementor-button') as $link){
+        if (pathinfo($link->href , PATHINFO_EXTENSION) == 'pdf') {
+            echo $link->href . PHP_EOL;
+        }
     }
 }
 
